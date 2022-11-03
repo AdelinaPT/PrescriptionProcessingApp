50923e1c9b009b30eff5287b4b7ec2df9150e59e
Adelina Platon <Adelina Platon@DESKTOP-ANK96PO>
2022-11-03 18:26:15 +0200

    Creat Modele

---
+++ PrescriptionProcessingApp/PrescriptionProcessingApp/Models/DBObjects/TipProdus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrescriptionProcessingApp.Models.DBObjects
+{
+    public partial class TipProdus
+    {
+        public TipProdus()
+        {
+            Produs = new HashSet<Produs>();
+        }
+
+        public Guid IdTipProdus { get; set; }
+        public string Categorie { get; set; } = null!;
+        public string? Detalii { get; set; }
+
+        public virtual ICollection<Produs> Produs { get; set; }
+    }
+}
