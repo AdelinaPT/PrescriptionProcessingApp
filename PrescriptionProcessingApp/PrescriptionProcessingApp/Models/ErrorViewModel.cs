0bb73a88641abcd9d90a7bf9f65c01b97967eb61
Adelina Platon <Adelina Platon@DESKTOP-ANK96PO>
2022-11-03 18:35:06 +0200

    Commit Test

--- PrescriptionProcessingApp/PrescriptionProcessingApp/Models/ErrorViewModel.cs
+++ PrescriptionProcessingApp/PrescriptionProcessingApp/Models/ErrorViewModel.cs
@@ -4,6 +4,8 @@ namespace PrescriptionProcessingApp.Models
     {
         public string? RequestId { get; set; }
 
+        public string Test { get; set; }
+
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
